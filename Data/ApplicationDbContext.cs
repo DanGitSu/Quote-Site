0f94a91daea31180a1b1a853deae65d72a9f33db
sudan <sudan@LAPTOP-DPH910O9>
2022-09-17 02:39:34 +1000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Goals_Site.Models;
+
+namespace Goals_Site.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Goals_Site.Models.Job> Job { get; set; }
+    }
+}
---
+++ Models/Job.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace Goals_Site.Models
+{
+    public class Job
+    {
+        public int JobID { get; set; }
+        public string? JobName { get; set; }
+        public string? Manager { get; set; }
+        public string? Client { get; set; }
+
+        public Job()
+        {
+
+        }
+    }
+    
+}
